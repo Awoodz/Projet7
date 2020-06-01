23d73e7a4cac19e5510f12f29a7062e3aa00da8e
Awoodz <a.bois77@gmail.com>
2020-06-01 18:21:13 +0200

    PY init flask

---
+++ config.py
@@ -0,0 +1,1 @@
+SECRET_KEY = "gbc$mxdcltr22n(@f8is7=*a)41gba9)q1jy*%9hpcmvkb+j20"
---
+++ grandpy/views.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('P7.html')
+
+
+@app.route('/', methods=['POST'])
+def get_form_input():
+    user_input = request.form['form_input']
+    print(user_input)
+    return render_template('P7.html')
+    
+
+app.config.from_object('config')
