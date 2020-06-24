d3889f3a66bd49a30b61af1670bb91d7962652af
Awoodz <a.bois77@gmail.com>
2020-06-24 15:27:09 +0200

    PY tests refacto - parser OK

--- grandpy/tests/test_parser.py
+++ grandpy/tests/test_parser.py
@@ -7,7 +7,7 @@ from grandpy.class_file.parser import Parser
 
 
 def test_do_parser_remove_stop_words():
-    form_input = (
+    text_to_parse = (
         "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
     )
     stopwords = hp.stop_words(
@@ -15,7 +15,7 @@ def test_do_parser_remove_stop_words():
         dt.SPECIAL_STOP_WORDS
     )
     punctuation = hp.punctuation(string.punctuation)
-    user = Parser(form_input, stopwords, punctuation)
-    assert user.parse(stopwords, punctuation) == (
+    parser_test = Parser(text_to_parse, stopwords, punctuation)
+    assert parser_test.request == (
         "salut connais adresse openclassrooms"
     )
